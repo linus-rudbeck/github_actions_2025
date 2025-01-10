0b54d1557230c79efb7273f2048e2b50408e5bd7
linus-rudbeck <linus.rudbeck@distansakademin.se>
2025-01-10 10:22:35 +0100

    Updated Main

--- src/main/java/se/distansakademin/Main.java
+++ src/main/java/se/distansakademin/Main.java
@@ -3,5 +3,6 @@ package se.distansakademin;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello Github Actions!!");
+        System.out.println("I'm a quick learner");
     }
 }
